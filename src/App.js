23f95e2cc0faefbbf9d05a001a0d01b51ee7b924
Rachel Eiting <reiting1@gmail.com>
2019-02-17 10:49:19 -0600

    First commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import FirstComponent from './FirstComponent';
 
 class App extends Component {
   render() {
@@ -11,6 +12,7 @@ class App extends Component {
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
+          <FirstComponent displaytext = 'First Component Data'/>
           <a
             className="App-link"
             href="https://reactjs.org"
